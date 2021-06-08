b04d265134a462daed29306e21772844625b77a5
Revati <revati.v.bordawekar@gmail.com>
2021-06-07 22:20:07 -0700

    new add

---
+++ anagram.js
@@ -0,0 +1,33 @@
+function anagrams(string1,string2){
+
+    let charMap1 = buildCharMap(string1);
+    let charMap2 = buildCharMap(string2);
+
+    if(Object.keys(charMap1).length !== Object.keys(charMap2).length){
+        console.log('Not an anagram');
+        return false;
+        
+    }
+
+    for(let char in charMap1){
+        if(charMap1[char] !== charMap2[char]){
+            console.log('Not an anagram');
+            return false;
+            
+        }
+    }
+    console.log('Anagram');
+    return true;
+   
+}
+
+function buildCharMap(str){
+    const charMap = {};
+    for (const char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
+
+anagrams("good morning", "good evening");
