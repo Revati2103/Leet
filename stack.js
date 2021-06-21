1294ea889b8f23dddb295e93ea9ccb2a5cde0891
Revati <revati.v.bordawekar@gmail.com>
2021-06-21 10:29:26 -0700

    new

---
+++ stack.js
@@ -0,0 +1,28 @@
+//Stack using javascript Arrays 
+
+class Stack {
+    constructor(){
+        this.data = [];
+    }
+    
+    push(record){
+        this.data.push(record);
+    }
+    
+    pop(){
+       return this.data.pop();
+    }
+    
+    peek(){
+        return this.data[this.data.length -1];
+    }
+
+}
+
+let s = new Stack();
+s.push(1);
+console.log(s);
+s.push(2);
+console.log(s);
+s.pop();
+console.log(s);
