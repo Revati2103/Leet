ad5f1c3d1555dfe23cf9d7834e0e80330889cd29
Revati <revati.v.bordawekar@gmail.com>
2021-07-23 11:46:21 -0700

    new file

---
+++ bubblesort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr){
+    for (let i = 0; i < arr.length; i++) {
+        for(let j=0; j< (arr.length - i); j++){
+            if (arr[j] > arr[j+1]){
+
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        
+    }
+
+    console.log("Sorted array is : ", arr);
+}
+
+bubbleSort([90,0,-10, 5,13]);
