12f2e2a55d31ea4f8f83a5319299954cc2fbdc71
Revati <revati.v.bordawekar@gmail.com>
2021-08-02 12:36:56 -0700

    updated

--- firstRepeatChar.js
+++ firstRepeatChar.js
@@ -4,17 +4,19 @@ function firstRepeatChar(word){
 let repeatChar = "";
 const charSet = new Set();
 
-for (const i of word) {
-   
-   charSet.add(i.toLowerCase());
-   
+
+for (const i of word.toLowerCase().split("")) {
+  
+
    if(charSet.has(i)){
        repeatChar = i;
        console.log(repeatChar);
        return repeatChar;
+   }else {
+    charSet.add(i);
    }
 }
 
 };
 
-firstRepeatChar('Apple');
+firstRepeatChar('Chandragiri');
