b093b289fb9b07039b90b2d94dd9d15d31fed0fc
Revati <revati.v.bordawekar@gmail.com>
2022-05-29 16:14:25 -0700

    updated

--- titleCase.js
+++ titleCase.js
@@ -1,13 +1,18 @@
 function titleCase(str){
 
-    let arr = str.toLowerCase().split(" ");
+    // let arr = str.toLowerCase().split(" ");
 
-    for(let i = 0; i< arr.length; i++){
+    // for(let i = 0; i< arr.length; i++){
 
-        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
-    }
+    //     arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    // }
 
-    return arr.join(" ");
+    // return arr.join(" ");
+
+    return str
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1));
     
 }
 
