86d5b915e6af4dee05b808ffde823dce972051fb
Revati <revati.v.bordawekar@gmail.com>
2022-05-29 16:11:05 -0700

    new

---
+++ titleCase.js
@@ -0,0 +1,14 @@
+function titleCase(str){
+
+    let arr = str.toLowerCase().split(" ");
+
+    for(let i = 0; i< arr.length; i++){
+
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+
+    return arr.join(" ");
+    
+}
+
+titleCase('I am a little tea pot');
