5f3e408b048de3da2d8b958679d7092c18789ab5
Revati <revati.v.bordawekar@gmail.com>
2022-05-29 16:41:31 -0700

    new file

---
+++ convertTo24Hours.js
@@ -0,0 +1,15 @@
+const convertTo24Hours = (timeText) => {
+    const timeTextLower = timeText.toLowerCase();
+    let [hours , mins] = timeTextLower.split(":");
+    
+        if(timeTextLower.endsWith("am")){
+            hours = hours === "12" ? "0" : hours;
+        } else if(timeTextLower.endsWith("pm")){
+            hours = hours === "12" ? hours : String(+hours +12);
+        }
+        
+    
+      return hours.padStart(2,0) + ":" + mins.slice(0,-2).padStart(2,0);
+    }
+    
+    convertTo24Hours("12:10AM")
