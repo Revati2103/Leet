122791fe25a07b871e55d2ce0292cf0b83b66737
Revati <revati.v.bordawekar@gmail.com>
2022-06-08 15:23:56 -0700

    new

---
+++ duplicateCheck.js
@@ -0,0 +1,14 @@
+function areThereDuplicates(...args) {
+    const set = new Set();
+    
+    if(!args.length) return false;
+    
+    for(let i = 0; i< args.length; i++){
+        if(set.has(args[i])) return true;
+        set.add(args[i]);
+    }
+    return false;
+   }
+
+//    areThereDuplicates(1,2,3) => false
+//    areThereDuplicates(1,1,2) => true
