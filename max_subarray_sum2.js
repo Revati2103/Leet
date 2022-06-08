604c9ca67f81a93a7b2514d4e7a57c9099a35c2c
Revati <revati.v.bordawekar@gmail.com>
2022-06-07 23:00:31 -0700

    new

---
+++ max_subarray_sum2.js
@@ -0,0 +1,21 @@
+function maxSubArraySum(arr, num){
+    let maxSum = 0;
+    let tempSum = 0;
+
+    if(arr.length < num) return null;
+
+    for(let i = 0; i<num ; i++){
+        maxSum += arr[i];
+    }
+
+    tempSum = maxSum;
+
+    for(let i = num; i< arr.length; i++){
+        tempSum = tempSum + arr[i] - arr[i-num];
+        maxSum = Math.max(tempSum, maxSum);
+    }
+
+    return maxSum;
+}
+
+maxSubArraySum([1,2,3,4,5], 2)
