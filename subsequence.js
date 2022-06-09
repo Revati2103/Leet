d97908fb44a42de23dab1be4e7c2fe87916b5455
Revati <revati.v.bordawekar@gmail.com>
2022-06-08 18:55:03 -0700

    new

---
+++ subsequence.js
@@ -0,0 +1,20 @@
+function isSubsequence(str1, str2) {
+    let i = 0;
+    let j = 0;
+    
+    if(!str1.length) return true;
+    
+    while(j<str2.length){
+        if(str1[i] === str2[j])  i++;
+        
+        if(i == str1.length) return true;
+        j++;
+    
+            
+    }
+    return false;
+  }
+
+//   isSubsequence('hello', 'hello world') => true;
+//   isSubsequence('sing', 'sting') => true;
+//   isSubsequence('abc' , 'acb') => false;
