112faa96304acfd086aca05b92a580ceeff445b7
Revati <revati.v.bordawekar@gmail.com>
2022-12-23 12:44:53 -0800

    permutation solved

---
+++ permutation.js
@@ -0,0 +1,27 @@
+
+// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+const permutate = (nums = []) => {
+  let result = [];
+
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+     }
+   }
+ }
+
+ permute(nums)
+
+ return result;
+}
