2e808cd5dc9bcacd4aae30ccc3587012d5d49e0c
Revati <61644975+Revati2103@users.noreply.github.com>
2023-04-04 06:54:11 -0700

    Create threesum.js

---
+++ threesum.js
@@ -0,0 +1,36 @@
+
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+// Notice that the solution set must not contain duplicate triplets.
+
+
+function threeSum(nums) {
+  nums.sort((a, b) => a - b);
+  const result = [];
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+    let j = i + 1;
+    let k = nums.length - 1;
+
+    while (j < k) {
+      const sum = nums[i] + nums[j] + nums[k];
+
+      if (sum === 0) {
+        result.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
+
+        while (j < k && nums[j] === nums[j - 1]) j++;
+        while (j < k && nums[k] === nums[k + 1]) k--;
+      } else if (sum < 0) {
+        j++;
+      } else {
+        k--;
+      }
+    }
+  }
+
+  return result;
+}
