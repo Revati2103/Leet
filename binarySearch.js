25c54e83e0810ec508f6d64136d6dd422e93f861
Revati <61644975+Revati2103@users.noreply.github.com>
2023-05-16 06:04:22 -0700

    Create binarySearch.js

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Array<number>} arr The input integer array to be searched.
+ * @param {number} target The target integer to search within the array.
+ * @return {number} The index of target element in the array, or -1 if not found.
+ */
+export default function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while(left <= right){
+    const mid = Math.floor((left+right)/2);
+
+    if(target < arr[mid]){
+      right = mid -1
+    }else if(target > arr[mid]){
+      left = mid + 1
+    }
+    else {
+      return mid;
+    }
+  }
+  return -1;
+}
