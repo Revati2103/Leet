06d13f934032e5046c6a6769085019f222b6740b
Revati <61644975+Revati2103@users.noreply.github.com>
2023-06-05 01:05:58 -0700

    Create repeatStringTimes.js

---
+++ repeatStringTimes.js
@@ -0,0 +1,14 @@
+Write Javascript code to reverse words in a paragraph
+
+function reverseParagraph(str){
+
+   
+
+    return str.split(' ').reverse().join(' ');
+    
+}
+undefined
+reverseParagraph("It was a beautiful day");
+'day beautiful a was It'
+
+
