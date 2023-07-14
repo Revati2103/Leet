a4b0af3ab41f88d01f0234e642f329da9b02dd98
Revati <61644975+Revati2103@users.noreply.github.com>
2023-07-14 06:42:51 -0700

    Create singleNumber.js

---
+++ singleNumber.js
@@ -0,0 +1,28 @@
+// In this lab, you will implement a function that solves the Single Number problem. You must develop a solution with linear runtime complexity and use only constant extra space. Your function should work on a non-empty array of integers nums, where every element appears twice except for one. Find that single one.
+
+// Problem Description
+// Given an array of integers (nums), your task is to find the single element that appears only once in the array. All the other elements appear twice in the array.
+
+// Examples
+// Input: nums = [2,2,1] Output: 1
+
+// Input: nums = [4,1,2,1,2] Output: 4
+
+// Input: nums = [1] Output: 1
+
+function singleNumber(nums){
+
+    let numMap = new Map()
+
+    for(let i = 0; i<nums.length; i++) {
+        numMap.has(nums[i]) ? numMap.set(nums[i], numMap.get(nums[i]) + 1) : numMap.set(nums[i] , 1);
+    }
+
+    console.log(numMap.values());
+    
+    for (const [key, val] of numMap.entries()) {
+    if (val === 1) {
+      return key;
+    }
+    }
+}
