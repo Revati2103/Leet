209102ef2575139e18f25b0ea4403fe09ba15742
Revati <61644975+Revati2103@users.noreply.github.com>
2023-07-23 06:54:10 -0700

    Create customFilter.js

---
+++ customFilter.js
@@ -0,0 +1,11 @@
+function customFilter(arr, callback) {
+  const filteredArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      filteredArray.push(arr[i]);
+    }
+  }
+
+  return filteredArray;
+}
