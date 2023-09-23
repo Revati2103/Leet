f0e4ec53f38b615be6e9e0db73ec97de72e229ff
Revati <61644975+Revati2103@users.noreply.github.com>
2023-09-23 05:49:24 +0530

    Update arrayIntersection.js

--- arrayIntersection.js
+++ arrayIntersection.js
@@ -1,5 +1,5 @@
 
-// Intersection of Two Arrays
+// Intersection of Two Arrays.
 
 //Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
 
